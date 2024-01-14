b7a71cb133d986941b5b936b0449e99d903e2f86
nhota <thien.hieu2299@gmail.com>
2024-01-14 14:33:43 +0700

    hi

--- app/projects/[title]/page.tsx
+++ app/projects/[title]/page.tsx
@@ -8,11 +8,11 @@ export default function project({ params }: { params: { title: string } }) {
 
     return (
         <main >
-            <div className="flex flex-col items-start px-16">
+            <div className="flex flex-col items-start px-4 md:px-16">
                 <Image
                     className="h-52 w-screen object-cover border-[0.35rem]  shadow-xl"
                     src={projectData.imageUrl} alt="" />
-                <div className="bg-white w-full rounded-md mt-5 flex flex-col items-start">
+                <div className="bg-white w-full rounded-md mt-5 flex flex-col items-start px-3 md:px-10">
                     <h1 className="mx-auto font-bold text-4xl pt-7 font-bg-gray-700">{projectData.title}</h1>
                     <p className="mx-auto text-sm text-gray-800 pb-10">{projectData.description}</p>
                     <div dangerouslySetInnerHTML={{ __html: projectData.html }} />
--- lib/data.ts
+++ lib/data.ts
@@ -68,7 +68,7 @@ export const projectsData = [
       "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
     tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
     imageUrl: corpcommentImg,
-    html: ""
+    html: `<a href="abc.com">abc</a>`
 
   },
   {
